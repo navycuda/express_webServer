c5ceff44f969b0e6763cdfd4e4a81ababbefb396
Shane <navysupra@gmail.com>
2022-08-08 03:28:40 +0000

    boilerplate code, some tinkering

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 /* Require */
+const express = require('express');
+const app = express();
+const Port = 3000;
+
 /* Tcp:Http */
 /* Arguments & Properties */
 /* Export Functions */
 /* Local Functions */
 /* Execution & Test Data */
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
 /* Exports */
